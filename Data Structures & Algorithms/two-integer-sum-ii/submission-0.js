470442bb4febed22b4de080e77fa16a4f74f9d6b
YashaswiniMM27 <123085682+YashaswiniMM27@users.noreply.github.com>
2026-07-31 15:40:03 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+        
+        while(l <= r){
+            const sum = nums[l] + nums[r];
+            if(sum === target){
+                return [l+1, r+1]
+            }
+            if(sum > target){
+                r--;
+            }else{
+                l++;
+            }
+        }
+    }
+}
